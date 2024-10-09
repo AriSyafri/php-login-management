6f6075186e3ea1e3b28ad6df096836afaf4bc698
AriSyafri <diamondThink15@gmail.com>
2024-10-09 11:06:15 +0700

    registration controller

--- app/App/View.php
+++ app/App/View.php
@@ -5,11 +5,15 @@ namespace ProgrammerZamanNow\Belajar\PHP\MVC\App;
 class View
 {
 
-    public static function render(string $view, $model)
-    {
+    public static function render(string $view, $model) {
         require __DIR__ . '/../View/header.php';
         require __DIR__ . '/../View/' . $view . '.php';
         require __DIR__ . '/../View/footer.php';
+    }
+
+    public static function redirect(string $url) {
+        header("Location: $url");
+        exit();
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -3,10 +3,18 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use ProgrammerZamanNow\Belajar\PHP\MVC\App\Router;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database;
 use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\HomeController;
+use ProgrammerZamanNow\Belajar\PHP\MVC\Controller\UserController;
 
 
+\ProgrammerZamanNow\Belajar\PHP\MVC\Config\Database::getConnection('prod');
+
+// Home controller
 Router::add('GET', '/', HomeController::class, 'index', []);
 
+// user controller
+Router::add('GET', '/users/register', UserController::class, 'register', []);
+Router::add('POST', '/users/register', UserController::class, 'postRegister', []);
 
 Router::run();
