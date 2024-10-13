8ef48614519f0655fcb92fe9b9e4974e59a6b5e9
AriSyafri <diamondThink15@gmail.com>
2024-10-13 23:59:38 +0700

    membuat unit test update repo user profile

--- test/Repository/UserRepositoryTest.php
+++ test/Repository/UserRepositoryTest.php
@@ -9,8 +9,12 @@ use ProgrammerZamanNow\Belajar\PHP\MVC\Domain\User;
 class UserRepositoryTest extends TestCase {
     
     private UserRepository $userRepository;
+    private SessionRepository $sessionRepository;
 
     protected function setUp(): void {
+        $this->sessionRepository = new SessionRepository(Database::getConnection());
+        $this->sessionRepository->deleteAll();    
+            
         $this->userRepository = new UserRepository(Database::getConnection());
         $this->userRepository->deleteAll();
     }
@@ -33,6 +37,25 @@ class UserRepositoryTest extends TestCase {
     public function testFindByIdNotFound(){
         $user = $this->userRepository->findById("notfound");
         self::assertNull($user);
+    }
+
+    public function testUpdate(){
+        $user = new User();
+        $user->id = "ari";
+        $user->name = "Ari";
+        $user->password = "Rahasia";
+
+        $this->userRepository->save($user);
+
+        $user->name = "Ujang";
+        $this->userRepository->update($user);
+
+        $result = $this->userRepository->findById($user->id);
+
+        self::assertEquals($user->id, $result->id);
+        self::assertEquals($user->name, $result->name);
+        self::assertEquals($user->password, $result->password);
+
     }
 
 
